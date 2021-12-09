907c46eaab97735d7ef44c3a53f5b45146bb4409
simaraconceicao <contato@simaraconceicao.com>
2021-12-08 22:35:04 -0300

    config production

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
+const PORT = process.env.PORT || 3333
+
+app.use(morgan('tiny'))
 
 //conectar o db
 const db = require('./src/data/database')
@@ -9,10 +14,10 @@ app.use(express.json())
 
 //usar a rota
 const vagasRouter = require('./src/routes/vagas.routes')
-app.use('/vagas', vagasRouter)
-
 const candidatasRouter = require('./src/routes/candidatas.routes')
-app.use('/candidatas', candidatasRouter)
 
-
+if (process.env.NODE_ENV === 'production') {
+  app.use('/vagas', vagasRouter)
+  app.use('/candidatas', candidatasRouter)
+}
 app.listen(PORT, console.log(`Server is starting at ${PORT}`))
